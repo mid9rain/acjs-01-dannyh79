b15d0889c4201e8a6dca1e399f5d9293d4391b69
Danny Cheng-Hsuan Han <30727089+dannyh79@users.noreply.github.com>
2019-04-21 13:28:11 +0800

    Update 01_two_fer.js
    clear comments inside the function

--- 01_two_fer.js
+++ 01_two_fer.js
@@ -2,8 +2,7 @@
 // 如果 name 是 undefined 或是 null 的話，回傳 "One for you, one for me"
 // 如果 name 是其它名字的話，那回傳 "One for 名字, one for me"
 function twoFer(name) {
-
-  // WiP - google how to check bool value
+  
   if ((name === null) || (name === undefined)) {
     return 'One for you, one for me';
   }
